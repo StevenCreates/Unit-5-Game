4f22b25e1459c1aa854ced5b994a6ee78130e685
Steven Hofheins <stevenisinsane@gmail.com>
2019-09-15 16:30:09 -0600

    addes psuedo code to js file

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+//document ready function
+$( document ).ready(function() {
+
+
+//declaring variables
+
+//creating questions
+
+//populating questions to html
+
+//creating timer 
+
+//capturing wins and losses
+
+//report results at the end (probably use the capturing wins and losses function and add them together)
+
+//end doc ready function
+});
